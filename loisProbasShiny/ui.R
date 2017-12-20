287b1f68cfe9feea4cc477d1dbc4a334a7bfb0ce
AlexisPoyen <poyen.alexis.info@gmail.com>
2017-12-20 12:11:03 +0100

    main frame

--- loisProbasShiny/ui.R
+++ loisProbasShiny/ui.R
@@ -10,21 +10,14 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Probabilités Antoine - Alexis"),
 
   # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      tabsetPanel(
+        tabPanel("Lois de Poisson", plotOutput('poissonPlot')),
+        tabPanel("Lois Normale", plotOutput('normalePlot'))
+      )
     )
-  )
 ))
